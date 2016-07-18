5898a4ae627fb1da3b72d10cbb06af09bfb4b349
Miron Jakubowski <mijakubowski@gmail.com>
2016-07-18 22:01:32 +0200

    home and base view

--- digs/settings.py
+++ digs/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'digs_web.apps.DigsWebConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
